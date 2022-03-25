e34a6ee3fc487918b34510d97fd8281022cdb49d
DeevanabilaUNJ <deevanabila_1306619030@mhs.unj.ac.id>
2022-03-25 15:55:36 +0700

    Commit Pertama

---
+++ halo_dunia.py
@@ -0,0 +1,5 @@
+Python 2.7.1 (r271:86832, Nov 27 2010, 18:30:46) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Halo dunia !")
+Halo dunia !
+>>> 
